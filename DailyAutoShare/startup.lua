3efdc639cac36a06e920c9c064e77e2f16ca77eb
git <vivec@telvanni.eu>
2018-04-10 07:37:13 +0200

    version number bump

--- DailyAutoShare/startup.lua
+++ DailyAutoShare/startup.lua
@@ -3,7 +3,7 @@ DAS                         = DailyAutoShare
 local DailyAutoShare        = DailyAutoShare
 
 DAS.name                    = "Daily Autoshare"
-DAS.version                 = "3.0"
+DAS.version                 = "3.0.5"
 DAS.author                  = "manavortex"
 DAS.settings                = {}
 DAS.globalSettings          = {}
