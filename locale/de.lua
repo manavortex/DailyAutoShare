7ba7aaa864f5ecbf4e677c1e8545ddf9ad317ae0
git <vivec@telvanni.eu>
2018-06-06 08:00:54 +0200

    added german quest names

--- locale/de.lua
+++ locale/de.lua
@@ -29,6 +29,22 @@ local strings  = {
 	DAS_SI_SETOPEN_TRUE	 	= "Toggle open",
 	DAS_SI_SETOPEN_FALSE	= "Toggle complete",
 	
+    DAS_ELF_RELIC           = "Relic Runaround",    
+    DAS_ELF_PILGR           = "Pilgrimage's End",    
+    DAS_ELF_LIGHT           = "Snuffing Out the Light",   
+    DAS_ELF_SERPE           = "Culling Serpents",
+    DAS_ELF_MEMO            = "Struck from Memory",
+    DAS_ELF_ROSE            = "A Rose's Beauty",
+    
+      
+    DAS_ELF_ALCHE           = "The Abyssal Alchemist",   
+    DAS_ELF_GRAVE           = "Never Forgotten",        
+    DAS_ELF_QUEEN           = "The Sickening Sea",    
+    DAS_ELF_GRIFFIN         = "Birds of a Feather",    
+    DAS_ELF_SNAKE           = "Run Aground",
+    DAS_ELF_WILD            = "Taming the Wild",
+    
+    DAS_ELF_GEYSER          = "Sinking Summerset", 
 	
 	-- Clockwork City
 	DAS_CLOCK_IMP		 = "Das Reizen des Unvollendeten", 
