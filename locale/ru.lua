6473346d728540a2cd0394de70141cdd3991ebce
Titouan Baron <tiitou22@gmail.com>
2021-06-09 01:52:09 +0200

    Update ru.lua
    To be translated, text must match perfectly to the name of the quest

--- locale/ru.lua
+++ locale/ru.lua
@@ -19,6 +19,7 @@ local strings  = {
 	DAS_SI_SPAM_SINGLE	 	= "Spam",
 	DAS_SI_SETOPEN_TRUE	 	= "Toggle open",
 	DAS_SI_SETOPEN_FALSE	= "Toggle complete",
+	DAS_SI_ACTIVATE_IN      = " Activate in zone : ",
 	-- Clockwork City
 	DAS_CLOCK_IMP			 = "Пробуждение Несовершенства",
 	DAS_CLOCK_FOE			 = "Враг в прекрасном оперении",
@@ -163,6 +164,23 @@ local strings  = {
     DAS_QUEST_CC_IGMUND     = "Igmund",
     DAS_QUEST_W_GURUZUG     = "Guruzug",
     DAS_QUEST_W_ARZORAG     = "Arzorag",
+--Blackwood to be translated
+--Boss dailies : NPC = Brita Silanus
+DAS_BLACKWD_TOAD	=	"Goblin Research",
+DAS_BLACKWD_RITUAL	=	"Rites of the Sul-Xan",
+DAS_BLACKWD_EXCAV	=	"Legend of the Man-Bull",
+DAS_BLACKWD_FROG	=	"A Reign of Frogs",
+DAS_BLACKWD_XANMEER	=	"Call of the Ruinach",
+DAS_BLACKWD_LAGOON	=	"The Trophy-Taker",
+DAS_QUEST_BW_BOSS	=	"Britta Silanus",     
+--Delve dailies : NPC = Deetum-Jas
+DAS_BLACKWD_BLOODRUN	=	"Shadow Tactics",
+DAS_BLACKWD_BEAUTY	=	"Beauty Amid Danger",
+DAS_BLACKWD_BLESS	=	"A Proper Blessing",
+DAS_BLACKWD_CHAIN	=	"Breaking the Chain",
+DAS_BLACKWD_INSECT	=	"Insect Savior",
+DAS_BLACKWD_CONFLICT	=	"Conflict of Interest",
+DAS_QUEST_BW_DELVE	=	"Deetum-Jas", 
 }
 DAS_STRINGS_LOCALE.ru = strings
 for stringId, stringValue in pairs(strings) do
