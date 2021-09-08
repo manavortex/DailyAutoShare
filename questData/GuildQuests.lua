3956fbcfe2f6f48ef12c59c3f1a2ca4bf7727338
mychaelo <mychaelo@live.com>
2021-09-08 16:04:19 +0300

    Split fighters and mages guild loops into individual quests
    1. Split loops into the individual quests, as the lists are not continuous, and don't support proper localisation
    2. Fix Grahtwood zoneId
    3. Comment out the bingo generation for now as it's not compatible with the current generation code

--- questData/GuildQuests.lua
+++ questData/GuildQuests.lua
@@ -20,7 +20,7 @@ DAS.subLists.guilds = {
   101,
   3,
   383,
-  104,
+  108,
   58,
   382,
   103,
@@ -37,52 +37,49 @@ DAS.subLists.ud = DAS.subLists.guilds
 DAS.QuestLists[zoneId2]  = DAS.QuestLists[zoneId]
 DAS.QuestLists[zoneId3]  = DAS.QuestLists[zoneId]
 local zoneIds = {
-    [104] = GetString(DAS_UD_ALIKR), -- Alik'r Desert
-    [381] = GetString(DAS_UD_AURID), -- Auridon
-    [92 ] = GetString(DAS_UD_BANGK), -- Bangkorai
-    [57 ] = GetString(DAS_UD_DESHA), -- Deshaan
-    [101] = GetString(DAS_UD_EASTM), -- Eastmarch
-    [3  ] = GetString(DAS_UD_GLENU), -- Glenumbra
-    [383] = GetString(DAS_UD_GRAHT), -- Grahtwood
-    [104] = GetString(DAS_UD_GREEN), -- Greenshade
-    [58 ] = GetString(DAS_UD_MALAB), -- Malabal Tor
-    [382] = GetString(DAS_UD_REAPE), -- Reaper's March
-    [103] = GetString(DAS_UD_RIFT),  -- The Rift
-    [20 ] = GetString(DAS_UD_RIVEN), -- Rivenspire
-    [117] = GetString(DAS_UD_SHADO), -- Shadowfen
-    [41 ] = GetString(DAS_UD_STONE), -- Stonefalls
-    [19 ] = GetString(DAS_UD_STORM), -- Stormhaven
+    [104] = {["fg"] = GetString(DAS_FG_ALIKR), ["mg"] = GetString(DAS_MG_ALIKR), ["ud"] = GetString(DAS_UD_ALIKR)}, -- Alik'r Desert
+    [381] = {["fg"] = GetString(DAS_FG_AURID), ["mg"] = GetString(DAS_MG_AURID), ["ud"] = GetString(DAS_UD_AURID)}, -- Auridon
+    [92 ] = {["fg"] = GetString(DAS_FG_BANGK), ["mg"] = GetString(DAS_MG_BANGK), ["ud"] = GetString(DAS_UD_BANGK)}, -- Bangkorai
+    [57 ] = {["fg"] = GetString(DAS_FG_DESHA), ["mg"] = GetString(DAS_MG_DESHA), ["ud"] = GetString(DAS_UD_DESHA)}, -- Deshaan
+    [101] = {["fg"] = GetString(DAS_FG_EASTM), ["mg"] = GetString(DAS_MG_EASTM), ["ud"] = GetString(DAS_UD_EASTM)}, -- Eastmarch
+    [3  ] = {["fg"] = GetString(DAS_FG_GLENU), ["mg"] = GetString(DAS_MG_GLENU), ["ud"] = GetString(DAS_UD_GLENU)}, -- Glenumbra
+    [383] = {["fg"] = GetString(DAS_FG_GRAHT), ["mg"] = GetString(DAS_MG_GRAHT), ["ud"] = GetString(DAS_UD_GRAHT)}, -- Grahtwood
+    [108] = {["fg"] = GetString(DAS_FG_GREEN), ["mg"] = GetString(DAS_MG_GREEN), ["ud"] = GetString(DAS_UD_GREEN)}, -- Greenshade
+    [58 ] = {["fg"] = GetString(DAS_FG_MALAB), ["mg"] = GetString(DAS_MG_MALAB), ["ud"] = GetString(DAS_UD_MALAB)}, -- Malabal Tor
+    [382] = {["fg"] = GetString(DAS_FG_REAPE), ["mg"] = GetString(DAS_MG_REAPE), ["ud"] = GetString(DAS_UD_REAPE)}, -- Reaper's March
+    [103] = {["fg"] = GetString(DAS_FG_RIFT),  ["mg"] = GetString(DAS_MG_RIFT),  ["ud"] = GetString(DAS_UD_RIFT)},  -- The Rift
+    [20 ] = {["fg"] = GetString(DAS_FG_RIVEN), ["mg"] = GetString(DAS_MG_RIVEN), ["ud"] = GetString(DAS_UD_RIVEN)}, -- Rivenspire
+    [117] = {["fg"] = GetString(DAS_FG_SHADO), ["mg"] = GetString(DAS_MG_SHADO), ["ud"] = GetString(DAS_UD_SHADO)}, -- Shadowfen
+    [41 ] = {["fg"] = GetString(DAS_FG_STONE), ["mg"] = GetString(DAS_MG_STONE), ["ud"] = GetString(DAS_UD_STONE)}, -- Stonefalls
+    [19 ] = {["fg"] = GetString(DAS_FG_STORM), ["mg"] = GetString(DAS_MG_STORM), ["ud"] = GetString(DAS_UD_STORM)}, -- Stormhaven
 }
-for id, udString in pairs(zoneIds) do
-    local zoneName = GetZoneNameByIndex(GetZoneIndex(id))
-    local fgString = GetString(DAS_GUILD_ANCHORS) .. zoneName
-    local mgString = GetString(DAS_GUILD_MADNESS) .. zoneName
+for id, guildStrings in pairs(zoneIds) do
     DAS.shareables[id]      = DAS.shareables[id]    or {}
     DAS.QuestLists[id]      = DAS.QuestLists[id]    or {}
     DAS.QuestLists[id].fg   = DAS.QuestLists[id].fg or {}
     DAS.QuestLists[id].mg   = DAS.QuestLists[id].mg or {}
     DAS.QuestLists[id].ud   = DAS.QuestLists[id].ud or {}
-    local bingoTable        = DAS.bingo[id]         or {}
-    table.insert(DAS.shareables[id],    fgString)
-    table.insert(bingoTable,            fgBingo)
-    table.insert(DAS.shareables[id],    mgString)
-    table.insert(bingoTable,            mgBingo)
-    table.insert(DAS.shareables[id],    udString)
-    table.insert(bingoTable,            udBingo)
-    DAS.makeBingoTable(id,              bingoTable)
-    table.insert(DAS.QuestLists[zoneId].fg, fgString)
-    table.insert(DAS.QuestLists[zoneId].mg, mgString)
-    table.insert(DAS.QuestLists[zoneId].ud, udString)
+    -- local bingoTable        = DAS.bingo[id]         or {}
+    table.insert(DAS.shareables[id],    guildStrings.fg)
+    -- table.insert(bingoTable,            fgBingo)
+    table.insert(DAS.shareables[id],    guildStrings.mg)
+    -- table.insert(bingoTable,            mgBingo)
+    table.insert(DAS.shareables[id],    guildStrings.ud)
+    -- table.insert(bingoTable,            udBingo)
+    -- DAS.makeBingoTable(id,              bingoTable)
+    table.insert(DAS.QuestLists[zoneId].fg, guildStrings.fg)
+    table.insert(DAS.QuestLists[zoneId].mg, guildStrings.mg)
+    table.insert(DAS.QuestLists[zoneId].ud, guildStrings.ud)
 end
 
 table.insert(tbl1, DAS.QuestLists[zoneId].fg)
-table.insert(tbl2, fgBingo)
+-- table.insert(tbl2, fgBingo)
 table.insert(tbl1, DAS.QuestLists[zoneId].mg)
-table.insert(tbl2, mgBingo)
+-- table.insert(tbl2, mgBingo)
 
 for idx, questName in pairs(DAS.QuestLists[zoneId].ud) do
     table.insert(tbl1, questName)
-    table.insert(tbl2, udBingo)
+    -- table.insert(tbl2, udBingo)
 end
 
 DAS.shareables[zoneId]  = tbl1
@@ -110,7 +107,7 @@ DAS.questFinisher[zoneId] = {
 DAS.questFinisher[zoneId2] = DAS.questFinisher[zoneId]
 DAS.questFinisher[zoneId3] = DAS.questFinisher[zoneId]
 DAS.questIds[zoneId] = {
-    -- guild
+    -- Undaunted
     [5733]  = true, -- Ancient Armaments in Bangkorai
     [5734]  = true, -- The Spirit Trap in Malabal Tor
     [5735]  = true, -- Ancestor Wards in Deshaan
@@ -123,14 +120,44 @@ DAS.questIds[zoneId] = {
     [5778]  = true, -- Give and Take in Shadowfen
     [5779]  = true, -- Icy Intrigue in Eastmarch
     [5798]  = true, -- Veiled Darkness in Auridon
-    [5800]  = true, --  Cursed Baubles of Stormhaven ??
+    [5800]  = true, -- Cursed Baubles of Stormhaven
     [5802]  = true, -- Inflamed Pyres of the Rift
     [5808]  = true, -- Darkness Blooms in Rivenspire
     [5853]  = true, -- Culinary Justice in Greenshade
+    
+    -- Mages Guild
+    [5814]  = true, -- Madness in Alik'r Desert
+    [5816]  = true, -- Madness in Auridon
+    [5818]  = true, -- Madness in Bangkorai
+    [5819]  = true, -- Madness in Deshaan
+    [5820]  = true, -- Madness in Eastmarch
+    [5822]  = true, -- Madness in Glenumbra
+    [5823]  = true, -- Madness in Grahtwood
+    [5824]  = true, -- Madness in Greenshade
+    [5825]  = true, -- Madness in Malabal Tor
+    [5826]  = true, -- Madness in Reaper's March
+    [5827]  = true, -- Madness in Rivenspire
+    [5828]  = true, -- Madness in Shadowfen
+    [5829]  = true, -- Madness in Stonefalls
+    [5830]  = true, -- Madness in Stormhaven
+    [5831]  = true, -- Madness in the Rift
+    
+    -- Fighters Guild
+    [5833]  = true, -- Dark Anchors in Glenumbra
+    [5784]  = true, -- Dark Anchors in Stormhaven
+    [5785]  = true, -- Dark Anchors in Rivenspire
+    [5786]  = true, -- Dark Anchors in Alik'r Desert
+    [5787]  = true, -- Dark Anchors in Bangkorai
+    [5788]  = true, -- Dark Anchors in Stonefalls
+    [5789]  = true, -- Dark Anchors in Deshaan
+    [5790]  = true, -- Dark Anchors in Shadowfen
+    [5791]  = true, -- Dark Anchors in Eastmarch
+    [5792]  = true, -- Dark Anchors in the Rift
+    [5793]  = true, -- Dark Anchors in Auridon
+    [5794]  = true, -- Dark Anchors in Grahtwood
+    [5795]  = true, -- Dark Anchors in Malabal Tor
+    [5796]  = true, -- Dark Anchors in Greenshade
+    [5797]  = true, -- Dark Anchors in Reaper's March
 }
-for questId=5814, 5830 do -- Madness in...
-    DAS.questIds[zoneId][questId] = true
-end
-for questId=5780, 5796 do -- Dark Anchors in...
-    DAS.questIds[zoneId][questId] = true
-end
+DAS.questIds[zoneId2] = DAS.questIds[zoneId]
+DAS.questIds[zoneId3] = DAS.questIds[zoneId]
