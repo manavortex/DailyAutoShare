82816a755cf13c84b0ffa51847b522e7c9734759
mychaelo <mychaelo@live.com>
2023-06-26 10:35:33 +0300

    Use a dynamic daily reset timer
    Also make sure to run the prep routines only once per load.
    
    Days are now saved per character, and quest data is also purged per character to support multiple reset times.

--- DasLog.lua
+++ DasLog.lua
@@ -1,29 +1,44 @@
-local day
+local dayCached
 local characterId = GetCurrentCharacterId()
 local typeString = "string"
+local clockBase = 1577858400
+local clockWasAdjusted = false
+
 
 ---Returns days since 2020-01-01 06:00 UTC rounded down to the nearest whole number
 ---@return number
 local function getToday()
-	return math.floor(GetDiffBetweenTimeStamps(GetTimeStamp(), 1577858400) / 86400)
+	-- Shift starting time to the current daily reset
+	if not clockWasAdjusted then
+		local clockOffset = GetDiffBetweenTimeStamps(GetTimeStamp() + GetTimeUntilNextDailyLoginRewardClaimS(), clockBase) % 86400
+		if clockOffset >= 43200 then clockOffset = clockOffset - 86400 end -- normalize to make EU clock the same day as NA
+		clockBase = clockBase + clockOffset -- should add -10800 on EU server, +14400 on NA/PTS [as of 2023]
+		clockWasAdjusted = true
+	end
+
+	return math.floor(GetDiffBetweenTimeStamps(GetTimeStamp(), clockBase) / 86400)
 end
 
 ---Returns today's quest log for the current player character
 ---@return table
 function DAS.GetShareableLog()
-	day = getToday()
-	local settings = DAS.globalSettings.completionLog or {}
-	-- purge the old entries
-	if settings._day ~= day then
-		settings = {}
-		settings._day = day
+	local day = getToday()
+	if nil == dayCached or dayCached ~= day then
+		local settings = DAS.globalSettings.completionLog or {}
+		-- initialize if empty
+		settings[characterId] = settings[characterId] or {}
+		-- pre4.5.0 compatibility, remove this in 4.5.1+
+		settings[characterId]._day = settings[characterId]._day or settings._day
+		-- purge the old entries
+		if settings[characterId]._day ~= day then
+			settings[characterId] = {}
+			-- store the day number per character
+			settings[characterId]._day = day
+		end
+		-- make sure it's set
+		DAS.globalSettings.completionLog = settings
+		dayCached = day
 	end
-	-- initialize if empty
-	settings[characterId] = settings[characterId] or {}
-	-- make sure it's set
-	DAS.globalSettings.completionLog = settings
-	DAS.todaysLog          = DAS.globalSettings.completionLog
-	DAS.todaysCharacterLog = DAS.globalSettings.completionLog[characterId]
 	return DAS.globalSettings.completionLog[characterId]
 end
 
@@ -45,7 +60,7 @@ end
 
 ---Get DAS status ID for the given quest
 ---@param questName string
----@return number
+---@return number|nil
 function DAS.GetQuestStatus(questName)
 	if nil == questName then return end
 	if nil ~= DAS.QuestNameTable[questName] then return DAS_STATUS_ACTIVE end
